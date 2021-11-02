5ef687cf5ba3225e6959ca13eba152096ed4e280
jhGomess <jhuangomes@hotmail.com>
2021-11-02 20:42:47 -0300

    fim do projeto

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ const linksSocialMedia = {
   twitter: "jh_gomess",
 };
 function changeSocialMediaLinks() {
-  userName.textContent = "jh";
-
   for (li of socialLinks.children) {
     const social = li.getAttribute("class");
 
@@ -19,6 +17,14 @@ changeSocialMediaLinks();
 
 function getGitHubProfileInfos() {
   const url = `https://api.github.com/users/${linksSocialMedia.github}`;
-  alert(url);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      userName.textContent = data.name;
+      userBio.textContent = data.bio;
+      userLink.href = data.html_url;
+      userImage.src = data.avatar_url;
+      userLogin.textContent = data.login;
+    });
 }
 getGitHubProfileInfos();
